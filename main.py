72686f921b36e342fef77ff51880a1d09c39b9c9
Bradly valadez <d00297297@dmail.dixie.edu>
2022-02-22 18:24:35 -0700

    tidy up comments/  formating

--- main.py
+++ main.py
@@ -34,23 +34,17 @@ def main():
 				bot.handleSafeExit()
 		return 0
 
+# Crashes on non-crash errors like invalid account balance
 def test():
 	while True:
 		bot = None
-		#try:
 		bot =	gmmBot.GMM_BOT(path, active_pair, sell_levels, buy_levels)
 		bot.trade(active_pair['base'], active_pair['quote'], active_pair['pair'], active_pair['order_min'], active_pair)
 			# Decay time is -.33/sec Trades/cancels add marks
 		time.sleep(3.2)
-		#except Exception as e:
-		#	print('main.py', 'exeption thrown on bot initializing or trade function')
-		#	print(e)
-		#finally:
-			# SAFEEXIT determines if open orders are before closing connection to API
-		#	if(SAFEEXIT and bot != None):
-		#		# TODO: Verify that this function has to be succesful or notification is given
-		#		bot.handleSafeExit()
 	return 0
+
+
 if __name__ == "__main__":
 		test()
 		#main()
